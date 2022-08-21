492bd3b8e29eb46da252dd3878611d2afee4292f
Andrey Ivanov <andrey-tm203@mail.ru>
2022-08-21 18:38:05 +0300

    realize upload avatar form

--- src/helpers/not.js
+++
@@ -1,3 +0,0 @@
-import Handlebars from 'handlebars/dist/handlebars.runtime';
-
-Handlebars.registerHelper('not', (condition) => !condition);
---
+++ src/pages/profileWithPopup/index.js
@@ -0,0 +1,34 @@
+import template from './profileWithPopup.hbs';
+import profileStyles from '../../modules/profile';
+import avatarFormStyles from '../../modules/avatarForm';
+import * as profilePageStyles from './profileWithPopup.module.css';
+import avatarStyles from '../../components/avatar';
+import buttonStyles from '../../components/button';
+import userDataStyles from '../../modules/userData';
+import arrowButtonStyles from '../../components/arrowButton';
+import inputStyles from '../../components/input';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const app = document.querySelector('#app');
+  app.classList.add(profilePageStyles.profilePage);
+
+  const user = {
+    login: 'Andrey',
+    name: 'Andrey',
+    secondName: 'Ivanov',
+    email: 'andrey.ivanov@gmail.ru',
+    phone: '+7 (999) 999 99 99',
+    displayedName: 'Andrey',
+  };
+
+  app.innerHTML = template({
+    avatarStyles,
+    userDataStyles,
+    profileStyles,
+    arrowButtonStyles,
+    avatarFormStyles,
+    inputStyles,
+    buttonStyles,
+    user,
+  });
+});
