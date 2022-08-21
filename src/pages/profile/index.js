e6465aea904e4ceae6e0a9e3bc13888e791eb8b4
Andrey Ivanov <andrey-tm203@mail.ru>
2022-08-21 14:39:49 +0300

    realize profile module

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -9,7 +9,20 @@ window.addEventListener('DOMContentLoaded', () => {
   const app = document.querySelector('#app');
   app.classList.add(profilePageStyles.profilePage);
 
+  const user = {
+    login: 'Andrey',
+    name: 'Andrey',
+    secondName: 'Ivanov',
+    email: 'andrey.ivanov@gmail.ru',
+    phone: '+7 (999) 999 99 99',
+    displayedName: 'Andrey',
+  };
+
   app.innerHTML = template({
-    avatarStyles, userDataStyles, profileStyles, inputStyles,
+    avatarStyles,
+    userDataStyles,
+    profileStyles,
+    inputStyles,
+    user,
   });
 });
