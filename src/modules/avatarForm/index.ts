4347a2a59fce79a61ce340390af07fb0e10f239a
Andrey Ivanov <andrey-tm203@mail.ru>
2023-01-15 22:45:44 +0700

    fix

--- src/modules/avatarForm/index.ts
+++
@@ -1,4 +0,0 @@
-import AvatarForm from './AvatarForm';
-export type { AvatarFormProps } from './AvatarForm';
-
-export default AvatarForm;
--- src/modules/passwordForm/index.ts
+++
@@ -1,4 +0,0 @@
-import PasswordForm from './PasswordForm';
-export type { PasswordFormProps } from './PasswordForm';
-
-export default PasswordForm;
